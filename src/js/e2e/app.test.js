4c039c6b89bb80aab97e6aafe4b9a0183b0d29ab
OlesyaZubkova <69164989+OlesyaZubkova@users.noreply.github.com>
2022-02-08 15:51:27 +0300

    Update app.test.js

--- src/js/e2e/app.test.js
+++ src/js/e2e/app.test.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 import { fork } from 'child_process';
 
-jest.setTimeout(40000);
+jest.setTimeout(60000);
 
 describe('button test', () => {
   let browser = null;
